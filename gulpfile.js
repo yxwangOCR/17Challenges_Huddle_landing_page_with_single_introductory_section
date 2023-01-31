253d493300d1eb5d5d1a6ffbad81a6498834dcb6
Yixuan Wang <wang.yixuan@live.fr>
2023-01-31 15:44:07 -0500

    Chore: setup project

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const {src, dest, watch, series} = require("gulp")
 const sass = require("gulp-sass")(require("sass"))
 
 function buildStyles(){
-    return src("index.scss").pipe(sass()).pipe(dest("css"))
+    return src("sass/**/*.scss").pipe(sass()).pipe(dest("css"))
 }
 
 function watchTasks(){
-watch(['index.scss'], buildStyles)
+watch(['sass/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles,watchTasks)
