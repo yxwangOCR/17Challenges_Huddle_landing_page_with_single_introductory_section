5bc4c062395721804a74cbc59971b03605b41a5e
Yixuan Wang <wang.yixuan@live.fr>
2023-02-01 18:28:49 -0500

    Chore: deploy website

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-const {src, dest, watch, series} = require("gulp")
+const { src, dest, watch, series } = require("gulp")
 const sass = require("gulp-sass")(require("sass"))
 
-function buildStyles(){
-    return src("sass/**/*.scss").pipe(sass()).pipe(dest("css"))
+function buildStyles() {
+    return src("sass/**/*.scss").pipe(sass()).pipe(dest("*.css"))
 }
 
-function watchTasks(){
-watch(['sass/**/*.scss'], buildStyles)
+function watchTasks() {
+    watch(['sass/**/*.scss'], buildStyles)
 }
 
-exports.default = series(buildStyles,watchTasks)
+exports.default = series(buildStyles, watchTasks)
